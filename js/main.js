f949c3bce6e21ee06f998fa40d2044add6457377
guilherme gg <guilhermegasse@gmail.com>
2023-04-01 05:59:39 -0300

    1° commit
    Primeira atualização, contendo o básico com a funcionalidade para os 3 primeiros botões.

---
+++ js/main.js
@@ -0,0 +1,13 @@
+function img01() {
+    document.getElementById("trocarimg").src = "img/emojis/emoji_u1f917.png";
+}
+
+function img02() {
+    document.getElementById("trocarimg").src = "img/emojis/emoji_u1f600.png";
+}
+
+function img03() {
+    document.getElementById("trocarimg").src = "img/emojis/emoji_u1f601.png";
+}
+
+console.log("Sucesso.");
